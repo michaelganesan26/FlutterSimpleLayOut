d6c679777b8a80ce40d6a758de32ca9670de01e3
Michael Ganesan <cookieganesan@gmail.com>
2019-03-06 08:49:12 -0800

    Added code to display Chen the Brew Master

--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,14 @@ class MyApp extends StatelessWidget {
     Widget textSection = Container(
       padding: const EdgeInsets.all(32),
       child: Text(
+        /*
         'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
             'Alps. Situated 1,578 meters above sea level, it is one of the '
             'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
             'half-hour walk through pastures and pine forest, leads you to the '
             'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
+            'enjoyed here include rowing, and riding the summer toboggan run.',*/
+            'Image of Chen the brew master from World of War Craft',
         softWrap: true,
       ),
     );
@@ -108,7 +110,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter layout demo',
+        title: 'Flutter Chen Image',
         theme: ThemeData(
           // This is the theme of your application.
           //
@@ -123,7 +125,7 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
             appBar: AppBar(
-              title: Text('Flutter layout demo'),
+              title: Text('Michael\'s Chen Image'),
             ),
             body: ListView(
               children: <Widget>[
